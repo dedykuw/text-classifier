e36ceef00842e979f790764a4cbfb0a6c435310c
dedy <dedykuw@gmail.com>
2017-06-14 02:38:19 +0800

    created text processor feature sanity check and dead code removal

--- index.js
+++ index.js
@@ -1,36 +1,76 @@
 var NaiveClassifier = require('./mymodule/NaiveClassifier');
-var naive = new NaiveClassifier();
-const readline = require('readline');
+const prompt = require('prompt');
+var config = {
+    caseFolding : 0,
+    escapeCharacter : 0,
+    stopword : 0,
+    stem : 0
+};
 
-naive.splitSgml();
-naive.on('splitEnd',function () {
-    naive.learnAll()
-});
-naive.on('learnEnd',function () {
-    naive.testAll();
-});
+var schema = {
+    properties: {
 
-
-naive.on('testAllEnd',function () {
-});
-
-naive.on('createXMLEnd',function () {
-    console.log('Classifying test file finished, please wait until output file written completely in to folder named \'output\' !!');
-    var inputText = '';
-    var rl = readline.createInterface(process.stdin, process.stdout);
-    rl.setPrompt(' \n you can  type your test data body text here (end it with \'end>\') in new line, \n or type \'exit1\' to quit > ');
-    rl.prompt();
-    rl.on('line', function(line) {
-        if (line === "exit1") rl.close();
-        inputText += line;
-        if (line === "end>"){
-            console.log('Topic : '+naive.testOne(inputText));
-            rl.prompt();
+        caseFolding: {
+            message: 'Use case folding (use = 1, no = 0) : ',
+            required: true
+        },
+        escapeCharacter: {
+            message: 'Use escape character (use = 1, no = 0) : ',
+            required: true
+        },
+        stopword: {
+            message: 'Use Stopword (use = 1, no = 0) : ',
+            required: true
+        },
+        stem: {
+            message: 'Use stemming (use = 1, no =0) : ',
+            required: true
         }
-    }).on('close',function(){
-        process.exit(0);
+    }
+};
+
+prompt.get(schema, function (err, result) {
+    config.caseFolding = parseInt(result.caseFolding);
+    config.escapeCharacter = parseInt(result.escapeCharacter);
+    config.stopword = parseInt(result.stopword);
+    config.stem = parseInt(result.stem);
+
+    var naive = new NaiveClassifier(config);
+    const readline = require('readline');
+
+    naive.splitSgml();
+    naive.on('splitEnd',function () {
+        naive.learnAll()
     });
-});
+    naive.on('learnEnd',function () {
+        naive.testAll();
+    });
+
+
+    naive.on('testAllEnd',function () {
+    });
+
+    naive.on('createXMLEnd',function () {
+        console.log('Classifying test file finished, please wait until output file written completely in to folder named \'output\' !!');
+        var inputText = '';
+        var rl = readline.createInterface(process.stdin, process.stdout);
+        rl.setPrompt(' \n you can  type your test data body text here (end it with \'end>\') in new line, \n or type \'exit1\' to quit > ');
+        rl.prompt();
+        rl.on('line', function(line) {
+            if (line === "exit1") rl.close();
+            inputText += line;
+            if (line === "end>"){
+                console.log('Topic : '+naive.testOne(inputText));
+                rl.prompt();
+            }
+        }).on('close',function(){
+            process.exit(0);
+        });
+    });
+
+
+})
+
 
 
 
--- mymodule/NaiveClassifier.js
+++ mymodule/NaiveClassifier.js
@@ -9,17 +9,37 @@ const bayes = require('bayes');
 const xml2js = require('xml2js');
 const parser = new xml2js.Parser();
 const builder = new xml2js.Builder();
+const stopWords = require('./stopwords');
+const Stem = require('./stem');
 
-function NaiveClassifier(){
+function NaiveClassifier(Config){
     events.EventEmitter.call(this);
     this.index = {};
     this.trainingData = [];
     this.testingData = [];
     this.classifier = bayes();
-    this.xml = ''
+    this.xml = '';
+    this.config = Config;
 }
 
 NaiveClassifier.prototype.__proto__ = events.EventEmitter.prototype;
+
+NaiveClassifier.prototype.textProcessor = function (body,config) {
+    return body.split(' ').map(function (data, index) {
+        if (config.caseFolding){
+            data = data.toLowerCase()
+        }else if(config.escapeCharacter) {
+            data = data.replace(/[^a-zA-Z0-9\u00C0-\u00FF]+/g, ' ').split(' ');
+        }else if(config.stopword){
+            data = data.filter(function(item) {
+                return stopWords.indexOf(item) === -1;
+            })
+        }else if(config.stem){
+            data = data.map(Stem)
+        }
+        return data;
+    }).join(' ');
+};
 
 NaiveClassifier.prototype.splitSgml = function () {
     console.log('Splitting SGML file in to Training and Testing data');
@@ -48,7 +68,7 @@ NaiveClassifier.prototype.splitSgml = function () {
             parser.parseString('<REUTERS '+doc, function (err, result) {
                 if (result != undefined && result != null) {
                     if (result.REUTERS){
-                        if (result.REUTERS.$.TOPICS == 'YES'){
+                        if (result.REUTERS.$.LEWISSPLIT=="TRAIN" ){
                             //tempThis.trainingData.push(result);
                             trainingDataWriteStreamer.write('<REUTERS '+doc+'\n');
                         }else {
@@ -66,7 +86,7 @@ NaiveClassifier.prototype.splitSgml = function () {
                 tempThis.emit('splitEnd');
                 trainingDataWriteStreamer.end();
                 testingDataWriteStreamer.end();
-            }, 20000)
+            }, 5000)
         }))
 
 
@@ -88,8 +108,10 @@ NaiveClassifier.prototype.learnAll =  function () {
 
                         result.REUTERS.TOPICS[0].D.forEach(function (TOPIC) {
                             if(result.REUTERS.TEXT[0].BODY != undefined && result.REUTERS.TEXT[0].BODY.length > 0) {
-                                tempThis.classifier.learn(result.REUTERS.TEXT[0].BODY[0], TOPIC);
-                                counter = counter+1;
+                                if (TOPIC){
+                                    tempThis.classifier.learn(tempThis.textProcessor(result.REUTERS.TEXT[0].BODY[0],tempThis.config), TOPIC);
+                                    counter = counter+1;
+                                }
                             }
                         });
                     }
@@ -103,12 +125,12 @@ NaiveClassifier.prototype.learnAll =  function () {
             setTimeout(function () {
                 tempThis.emit('learnEnd');
                 console.log(counter+ ' Documents learned');
-            },20000)
+            },5000)
         }))
 
 };
 NaiveClassifier.prototype.testOne = function (body) {
-    return this.classifier.categorize(body)
+    return this.classifier.categorize(this.textProcessor(body,this.config))
 };
 NaiveClassifier.prototype.testAll = function () {
     console.log('Now testing all test data!! ');
@@ -144,7 +166,7 @@ NaiveClassifier.prototype.testAll = function () {
                 tempThis.emit('testAllEnd');
                 tempThis.emit('createXMLEnd');
 
-            },20000)
+            },5000)
         }))
 
 };
